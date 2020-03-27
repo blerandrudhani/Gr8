887acd330dae65931616a5acf1d0263c460221f3
Drilonii <58754697+Drilonii@users.noreply.github.com>
2020-03-27 22:47:51 +0100

    Update dS.cs

--- dS.cs
+++ dS.cs
@@ -285,6 +285,72 @@ class morse
 
         return "";
     }
+    public char morseDeco(string text)
+    {
+        switch (text)
+        {
+            case ".- ":
+                return 'a';
+
+            case "-... ":
+                return 'b';
+
+            case "-.-. ":
+                return 'c';
+
+            case "-.. ":
+                return 'd';
+            case ". ":
+                return 'e';
+            case "..-. ":
+                return 'f';
+            case "--. ":
+                return 'g';
+            case ".... ":
+                return 'h';
+            case ".. ":
+                return 'i';
+            case ".--- ":
+                return 'j';
+            case "-.- ":
+                return 'k';
+            case ".-.. ":
+                return 'l';
+            case "-- ":
+                return 'm';
+            case "-. ":
+                return 'n';
+            case "--- ":
+                return 'o';
+            case ".--. ":
+                return 'p';
+            case "--.- ":
+                return 'q';
+            case ".-. ":
+                return 'r';
+            case "... ":
+                return 's';
+            case "- ":
+                return 't';
+            case "..- ":
+                return 'u';
+            case "...- ":
+                return 'v';
+            case ".-- ":
+                return 'w';
+            case "-..- ":
+                return 'x';
+            case "-.-- ":
+                return 'y';
+            // for space 
+            case "--.. ":
+                return 'z';
+        }
+
+
+        return ' ';
+    }
+}
     
     
                 
